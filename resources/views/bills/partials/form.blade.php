11446260031a4bf62a273ee13490f554dd074947
bomj321 <mauricio1314@hotmail.com>
2019-04-27 19:52:50 -0400

    Generando Facturas

--- resources/views/bills/partials/form.blade.php
+++ resources/views/bills/partials/form.blade.php
@@ -32,3 +32,61 @@
 	</div>
 	
 </div>
+
+
+<div class="row">
+	<div class="col-md-6">
+		<div class="form-group">
+			    {{ Form::label('Invoice_num', 'Nro. de Factura') }}
+	            {{ Form::text('Invoice_num', null, ['class' => 'form-control', 'id' => 'Invoice_num']) }}
+		</div>
+	</div>
+
+	<div class="col-md-6">
+		<div class="form-group">
+			    {{ Form::label('Invoice_date', 'Fecha de Factura') }}
+				{!! Form::date('Invoice_date', \Carbon\Carbon::now(),['class' => 'form-control']) !!}
+		</div>
+	</div>
+	
+</div>
+
+
+<div class="row">
+	<div class="col-md-6">
+		<div class="form-group">
+			     {{ Form::label('invoice_currency_code', 'Proveedores') }}
+				 {!! Form::select('invoice_currency_code',['PEN' => 'PEN','USD' => 'USD'],null,['class' => 'form-control']) !!}
+		</div>
+	</div>
+
+	<div class="col-md-6">
+		<div class="form-group">
+			    {{ Form::label('exchange_rate', 'Tipo de Cambio') }}
+				{{ Form::text('exchange_rate', 1, ['class' => 'form-control', 'id' => 'exchange_rate','readonly']) }}
+		</div>
+	</div>
+	
+</div>
+
+
+<div class="row">
+	<div class="col-md-6">
+		<div class="form-group">
+			    {{ Form::label('description', 'Glosa') }}
+	            {{ Form::text('description', null, ['class' => 'form-control', 'id' => 'description']) }}
+		</div>
+	</div>
+
+	<div class="col-md-6">
+		<div class="form-group">
+			    {{ Form::label('ap_tax_codes_all', 'Código Impuesto') }}
+				 {!! Form::select('ap_tax_codes_all',['IGV18' => 'IGV18','IGV17' => 'IGV17'],null,['class' => 'form-control']) !!}
+		</div>
+	</div>
+	
+</div>
+
+<div class="form-group">
+	{{ Form::submit('Generar Factura', ['class' => 'btn btn-sm btn-danger']) }}
+</div>
