3ca14c708af17a68db49c3e9c6b94912515b401d
bomj321 <mauricio1314@hotmail.com>
2019-05-10 16:13:16 -0400

    Creando seccion de lineas de factura

---
+++ app/Http/Controllers/LinesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LinesController extends Controller
+{
+     public function index()
+    {
+
+
+
+       echo "SITIO EN CONSTRUCCION";
+        
+    }
+}
